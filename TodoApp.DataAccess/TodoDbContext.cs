fa3d8f3ecc0e59b540503e8ac08749d6da980ab9
AzamatVaxobov <azamatvaxobov104@gmail.com>
2025-07-02 13:08:40 +0500

    Add project files.

---
+++ TodoApp.DataAccess/TodoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.DataAccess.Entities;
+
+namespace DataAccess;
+
+public class TodoDbContext : DbContext
+{
+    public DbSet<Todo> Todos { get; set; }
+
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Optional: Customize your schema if required
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Todo>().ToTable("Todos");
+    }
+}
---
+++ TodoApp.Server/Configurations/DatabaseConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.DataAccess;
+
+namespace TodoApp.Server.Configurations
+{
+    public static class DatabaseConfiguration
+    {
+        public static void ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TodoDbContext>(options =>
+                options.UseMySql(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    new MySqlServerVersion(new Version(8, 0, 42)) // match your version
+                ));
+        }
+    }
+}
